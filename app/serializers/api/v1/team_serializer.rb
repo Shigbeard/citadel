dbbae26b67bcfd3de80c9ef0996601408cf07162
Shigbeard <4262327+Shigbeard@users.noreply.github.com>
2025-01-27 22:40:37 +1100

    Enhance team serializer to include captains This unfortunately bypasses the user serializer

--- app/serializers/api/v1/team_serializer.rb
+++ app/serializers/api/v1/team_serializer.rb
@@ -8,7 +8,25 @@ module API
       attribute(:avatar_thumb_url) { object.avatar.thumb.url }
       attribute(:avatar_icon_url) { object.avatar.icon.url }
 
-      has_many :users, key: :players, serializer: UserSerializer
+      # has_many :users, key: :players, serializer: UserSerializer
+      attribute :players do
+        object.users.map do |user|
+          {
+            id: user.id,
+            name: user.name,
+            description: user.description,
+            created_at: user.created_at,
+            steam_32: user.steam_32,
+            steam_64: user.steam_64,
+            steam_id3: user.steam_id3,
+            steam_64_str: user.steam_64.to_s,
+            profile_url: user.avatar.thumb.url,
+            captain: user.can?(:edit, object)
+          }
+        end
+      end
+
+
       has_many :rosters, serializer: V1::Leagues::RosterSerializer
     end
   end
--- app/serializers/api/v1/user_serializer.rb
+++ app/serializers/api/v1/user_serializer.rb
@@ -8,8 +8,20 @@ module API
       attribute(:steam_64_str) { object.steam_64.to_s } # For dumb json implementations
       attribute(:profile_url) { object.avatar.thumb.url }
 
+      attribute :captain, if: :team_context? do
+        can_edit_team?(instance_options[:team])
+      end
+
       has_many :teams, serializer: API::V1::TeamSerializer
       has_many :rosters, serializer: API::V1::Leagues::RosterSerializer
+
+      def team_context?
+        instance_options[:team].present?
+      end
+
+      def can_edit_team?(team)
+        object.can?(:edit, team) && object.can?(:use, :teams)
+      end
     end
   end
 end
